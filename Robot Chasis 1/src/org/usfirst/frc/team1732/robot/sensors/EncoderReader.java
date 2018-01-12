36faa7604634d4d62182b5ea8a82e830a8fa1ab0
Jay <Jay@LAPTOP-6HVHN0U7>
2018-01-12 07:07:25 -0600

    added comment

--- Robot Chasis 1/src/org/usfirst/frc/team1732/robot/sensors/EncoderReader.java
+++ Robot Chasis 1/src/org/usfirst/frc/team1732/robot/sensors/EncoderReader.java
@@ -1,5 +1,9 @@
 package org.usfirst.frc.team1732.robot.sensors;
 
+/*
+ * Why have a seperate reader class? So that multiple copies can be made and "zeroed" so we don't have to save certain positional data like we did in some autos last year
+ */
+
 public class EncoderReader {
 
 	private final EncoderBase e;
