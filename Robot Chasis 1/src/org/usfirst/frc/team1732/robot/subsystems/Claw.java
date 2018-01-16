1a171172eefc0f642ac24e8fb8e5d8d8b29b9178
Jay <Jay@LAPTOP-6HVHN0U7>
2018-01-16 00:44:20 -0600

    Finished claw class, need to determine current limit

--- Robot Chasis 1/src/org/usfirst/frc/team1732/robot/subsystems/Claw.java
+++ Robot Chasis 1/src/org/usfirst/frc/team1732/robot/subsystems/Claw.java
@@ -1,19 +1,21 @@
 package org.usfirst.frc.team1732.robot.subsystems;
 
-import java.util.function.DoubleSupplier;
+import java.util.function.Supplier;
 
 import org.usfirst.frc.team1732.robot.config.Node;
 
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Claw extends Subsystem {
 
 	public final Spark leftSpark;
 	public final Spark rightSpark;
-	public final DoubleSupplier leftSparkCurrent;
-	public final DoubleSupplier rightSparkCurrent;
+	public final Supplier<Double> leftSparkCurrent;
+	public final Supplier<Double> rightSparkCurrent;
+
 	private final PowerDistributionPanel panel = new PowerDistributionPanel();
 	public static final double currentLimit = 10;
 
@@ -33,8 +35,22 @@ public class Claw extends Subsystem {
 		set(-0.5, -0.5);
 	}
 
+	public void setHold() {
+		set(-0.1, -0.1);
+	}
+
 	public void setOut() {
 		set(0.5, 0.5);
+	}
+
+	public void setStop() {
+		set(0, 0);
+	}
+
+	@Override
+	public void periodic() {
+		SmartDashboard.putNumber("Left Spark Current", leftSparkCurrent.get());
+		SmartDashboard.putNumber("Right Spark Current", rightSparkCurrent.get());
 	}
 
 	@Override
