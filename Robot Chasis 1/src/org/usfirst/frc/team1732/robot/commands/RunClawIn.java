a8d1a08f9c1342af6155de7455347597f9f52aad
Jay <Jay@LAPTOP-6HVHN0U7>
2018-01-16 00:46:04 -0600

    Changed run claw in to use current limit

--- Robot Chasis 1/src/org/usfirst/frc/team1732/robot/commands/RunClawIn.java
+++ Robot Chasis 1/src/org/usfirst/frc/team1732/robot/commands/RunClawIn.java
@@ -1,13 +1,14 @@
 package org.usfirst.frc.team1732.robot.commands;
 
 import org.usfirst.frc.team1732.robot.Robot;
+import org.usfirst.frc.team1732.robot.subsystems.Claw;
 
-import edu.wpi.first.wpilibj.command.InstantCommand;
+import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
-public class RunClawIn extends InstantCommand {
+public class RunClawIn extends Command {
 
 	public RunClawIn() {
 		requires(Robot.claw);
@@ -16,6 +17,16 @@ public class RunClawIn extends InstantCommand {
 	// Called just before this Command runs the first time
 	protected void initialize() {
 		Robot.claw.setIn();
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return Math.abs(Robot.claw.leftSparkCurrent.get()) > Claw.currentLimit
+				|| Math.abs(Robot.claw.rightSparkCurrent.get()) > Claw.currentLimit;
+	}
+
+	protected void end() {
+		Robot.claw.setStop();
 	}
 
 }
