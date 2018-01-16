6f5f1f311065791f5fff8f100fe7b9ab8d6d49ac
Jay <Jay@LAPTOP-6HVHN0U7>
2018-01-16 00:42:43 -0600

    Setup arm class, need to determine field values (empirical)

--- Robot Chasis 1/src/org/usfirst/frc/team1732/robot/subsystems/Arm.java
+++ Robot Chasis 1/src/org/usfirst/frc/team1732/robot/subsystems/Arm.java
@@ -2,18 +2,34 @@ package org.usfirst.frc.team1732.robot.subsystems;
 
 import org.usfirst.frc.team1732.robot.Robot;
 import org.usfirst.frc.team1732.robot.config.Node;
+import org.usfirst.frc.team1732.robot.sensors.TalonEncoder;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Arm extends Subsystem {
 
 	public final TalonSRX talon;
+	public final TalonEncoder encoder;
+	public static final double DEGREES_PER_PULSE = 10;
+
+	public static enum ArmPositions {
+		SCORING(0), UPRIGHT(0), HORIZONTAL(0), INTAKE(0);
+
+		public final double position;
+
+		private ArmPositions(double d) {
+			position = d;
+		}
+	}
 
 	public Arm(Node armNode) {
 		talon = MotorUtils.configureTalon(armNode.getNode("talon"), 0, Robot.CONFIG_TIMEOUT);
+		encoder = new TalonEncoder(talon);
+		encoder.setDistancePerPulse(DEGREES_PER_PULSE);
 	}
 
 	public void setUp() {
@@ -35,6 +51,11 @@ public class Arm extends Subsystem {
 	@Override
 	protected void initDefaultCommand() {
 
+	}
+
+	@Override
+	public void periodic() {
+		SmartDashboard.putNumber("Arm Angle", encoder.getPosition());
 	}
 
 }
