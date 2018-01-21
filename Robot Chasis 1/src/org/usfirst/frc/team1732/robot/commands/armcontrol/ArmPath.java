b38727d54bbedb3e06bb0b99f741d1cdf638a377
Jay Schauer <lacrossejason@gmail.com>
2018-01-20 19:41:50 -0600

    changed Armpath to accept doubles, and fixed end reset

--- Robot Chasis 1/src/org/usfirst/frc/team1732/robot/commands/armcontrol/ArmPath.java
+++ Robot Chasis 1/src/org/usfirst/frc/team1732/robot/commands/armcontrol/ArmPath.java
@@ -12,9 +12,13 @@ import edu.wpi.first.wpilibj.command.Command;
 public class ArmPath extends Command {
 
 	public final double START_VOLTAGE, END_VOLTAGE, BIAS;
-	public final Arm.ArmPositions START_POSITION, END_POSITION;
+	public final double START_POSITION, END_POSITION;
 
 	public ArmPath(double startVolt, double endVolt, Arm.ArmPositions startPos, Arm.ArmPositions endPos, double bias) {
+		this(startVolt, endVolt, startPos.position, endPos.position, bias);
+	}
+
+	public ArmPath(double startVolt, double endVolt, double startPos, double endPos, double bias) {
 		requires(Robot.arm);
 		START_VOLTAGE = startVolt;
 		START_POSITION = startPos;
@@ -27,29 +31,29 @@ public class ArmPath extends Command {
 	@Override
 	protected void initialize() {
 		Robot.arm.setSpeed(START_VOLTAGE);
-		if (START_POSITION.equals(Arm.ArmPositions.INTAKE)) {
+		if (START_POSITION == Arm.ArmPositions.INTAKE.position && Robot.arm.encoder.getPosition() < 5) {
 			Robot.arm.encoder.zero();
 		}
 	}
 
 	@Override
 	protected void execute() {
-		Robot.arm.setSpeed(BasicControl.cosineInterpolate(START_VOLTAGE, START_POSITION.position, END_VOLTAGE,
-				END_POSITION.position, Robot.arm.encoder.getPosition()));
+		Robot.arm.setSpeed(BasicControl.cosineInterpolate(START_VOLTAGE, START_POSITION, END_VOLTAGE,
+				END_POSITION, Robot.arm.encoder.getPosition()));
 		// Robot.arm.setSpeed(Robot.arm.getTorqueCompensation(Robot.arm.encoder.getPosition())
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
 	@Override
 	protected boolean isFinished() {
-		return Math.abs(Robot.arm.encoder.getPosition()) > Math.abs(END_POSITION.position - BIAS);
+		return Math.abs(Robot.arm.encoder.getPosition()) > Math.abs(END_POSITION - BIAS);
 	}
 
 	// Called once after isFinished returns true
 	@Override
 	protected void end() {
 		Robot.arm.setStop();
-		if (END_POSITION.equals(Arm.ArmPositions.INTAKE)) {
+		if (END_POSITION == Arm.ArmPositions.INTAKE.position && Robot.arm.encoder.getPosition() < 5) {
 			Robot.arm.encoder.zero();
 		}
 	}
