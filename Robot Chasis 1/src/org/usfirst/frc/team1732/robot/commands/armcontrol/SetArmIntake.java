3029df4a3cf87f927372eadfc94b6f3c4c823e40
Jay Schauer <lacrossejason@gmail.com>
2018-01-20 19:38:40 -0600

    changed voltages to be smoother

--- Robot Chasis 1/src/org/usfirst/frc/team1732/robot/commands/armcontrol/SetArmIntake.java
+++ Robot Chasis 1/src/org/usfirst/frc/team1732/robot/commands/armcontrol/SetArmIntake.java
@@ -10,8 +10,8 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class SetArmIntake extends CommandGroup {
 
 	public SetArmIntake() {
-		addSequential(new ArmPath(-0.3, -0.31, ArmPositions.SCORE, ArmPositions.UPRIGHT, 0));
-		addSequential(new ArmPath(-0.3, -0.0, ArmPositions.UPRIGHT, ArmPositions.HORIZONTAL, 0));
-		addSequential(new ArmPath(-0.0, 0, ArmPositions.HORIZONTAL, ArmPositions.INTAKE, -5));
+		addSequential(new ArmPath(-0.3, -0.5, ArmPositions.SCORE, ArmPositions.UPRIGHT, 0));
+		addSequential(new ArmPath(-0.5, 0.05, ArmPositions.UPRIGHT, ArmPositions.HORIZONTAL, 0));
+		addSequential(new ArmPath(0.05, 0, ArmPositions.HORIZONTAL, ArmPositions.INTAKE, -5));
 	}
 }
