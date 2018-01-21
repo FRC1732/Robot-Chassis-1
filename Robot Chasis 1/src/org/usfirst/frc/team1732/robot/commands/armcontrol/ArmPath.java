3c4caf7ad50148b236cf37966f623c636bda36f0
Jay Schauer <lacrossejason@gmail.com>
2018-01-20 21:00:33 -0600

    fixed going down and going up isFinished

--- Robot Chasis 1/src/org/usfirst/frc/team1732/robot/commands/armcontrol/ArmPath.java
+++ Robot Chasis 1/src/org/usfirst/frc/team1732/robot/commands/armcontrol/ArmPath.java
@@ -11,49 +11,56 @@ import edu.wpi.first.wpilibj.command.Command;
  */
 public class ArmPath extends Command {
 
-	public final double START_VOLTAGE, END_VOLTAGE, BIAS;
-	public final double START_POSITION, END_POSITION;
+	private final double startVoltage, endVoltage, bias;
+	private final double startPosition, endPosition;
+	private final boolean goingDown;
 
-	public ArmPath(double startVolt, double endVolt, Arm.ArmPositions startPos, Arm.ArmPositions endPos, double bias) {
-		this(startVolt, endVolt, startPos.position, endPos.position, bias);
+	public ArmPath(double startVolt, double endVolt, Arm.ArmPositions startPos, Arm.ArmPositions endPos, double bias,
+			boolean goingDown) {
+		this(startVolt, endVolt, startPos.position, endPos.position, bias, goingDown);
 	}
 
-	public ArmPath(double startVolt, double endVolt, double startPos, double endPos, double bias) {
+	public ArmPath(double startVolt, double endVolt, double startPos, double endPos, double bias, boolean goingDown) {
 		requires(Robot.arm);
-		START_VOLTAGE = startVolt;
-		START_POSITION = startPos;
-		END_VOLTAGE = endVolt;
-		END_POSITION = endPos;
-		BIAS = bias;
+		startVoltage = startVolt;
+		startPosition = startPos;
+		endVoltage = endVolt;
+		endPosition = endPos;
+		this.bias = bias;
+		this.goingDown = goingDown;
 	}
 
 	// Called just before this Command runs the first time
 	@Override
 	protected void initialize() {
-		Robot.arm.setSpeed(START_VOLTAGE);
-		if (START_POSITION == Arm.ArmPositions.INTAKE.position && Robot.arm.encoder.getPosition() < 5) {
+		Robot.arm.setSpeed(startVoltage);
+		if (startPosition == Arm.ArmPositions.INTAKE.position && Robot.arm.encoder.getPosition() < 5) {
 			Robot.arm.encoder.zero();
 		}
 	}
 
 	@Override
 	protected void execute() {
-		Robot.arm.setSpeed(BasicControl.cosineInterpolate(START_VOLTAGE, START_POSITION, END_VOLTAGE,
-				END_POSITION, Robot.arm.encoder.getPosition()));
+		Robot.arm.setSpeed(BasicControl.cosineInterpolate(startVoltage, startPosition, endVoltage,
+				endPosition, Robot.arm.encoder.getPosition()));
 		// Robot.arm.setSpeed(Robot.arm.getTorqueCompensation(Robot.arm.encoder.getPosition())
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
 	@Override
 	protected boolean isFinished() {
-		return Math.abs(Robot.arm.encoder.getPosition()) > Math.abs(END_POSITION - BIAS);
+		if (goingDown) {
+			return Math.abs(Robot.arm.encoder.getPosition()) < Math.abs(endPosition - bias);
+		} else {
+			return Math.abs(Robot.arm.encoder.getPosition()) > Math.abs(endPosition - bias);
+		}
 	}
 
 	// Called once after isFinished returns true
 	@Override
 	protected void end() {
 		Robot.arm.setStop();
-		if (END_POSITION == Arm.ArmPositions.INTAKE.position && Robot.arm.encoder.getPosition() < 5) {
+		if (endPosition == Arm.ArmPositions.INTAKE.position && Robot.arm.encoder.getPosition() < 5) {
 			Robot.arm.encoder.zero();
 		}
 	}
