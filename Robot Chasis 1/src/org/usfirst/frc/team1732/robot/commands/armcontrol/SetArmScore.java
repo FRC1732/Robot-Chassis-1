baded36974cf31e2d39c6f0be8206e453f411f6c
Jay Schauer <lacrossejason@gmail.com>
2018-01-20 21:01:04 -0600

    corrected setarmscore and setarmintake with goingdown

--- Robot Chasis 1/src/org/usfirst/frc/team1732/robot/commands/armcontrol/SetArmScore.java
+++ Robot Chasis 1/src/org/usfirst/frc/team1732/robot/commands/armcontrol/SetArmScore.java
@@ -7,9 +7,9 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class SetArmScore extends CommandGroup {
 
 	public SetArmScore() {
-		addSequential(new ArmPath(0.3, 1, ArmPositions.INTAKE, ArmPositions.HORIZONTAL, 0));
-		addSequential(new ArmPath(1, 0.5, ArmPositions.HORIZONTAL, ArmPositions.UPRIGHT, 0));
-		addSequential(new ArmPath(0.4, -0.1, ArmPositions.UPRIGHT, ArmPositions.SCORE, 5));
+		addSequential(new ArmPath(0.3, 1, ArmPositions.INTAKE, ArmPositions.HORIZONTAL, 0, false));
+		addSequential(new ArmPath(1, 0.5, ArmPositions.HORIZONTAL, ArmPositions.UPRIGHT, 0, false));
+		addSequential(new ArmPath(0.4, -0.1, ArmPositions.UPRIGHT, ArmPositions.SCORE, 5, false));
 	}
 
 }
