d5cd01c4910f2d357c8bc0379adfa8e403cc39f1
Jay Schauer <lacrossejason@gmail.com>
2018-02-10 17:16:29 -0600

    bla

--- Robot Chasis 1/src/org/usfirst/frc/team1732/robot/commands/TestPathing.java
+++ Robot Chasis 1/src/org/usfirst/frc/team1732/robot/commands/TestPathing.java
@@ -1,7 +1,5 @@
 package org.usfirst.frc.team1732.robot.commands;
 
-import java.util.Iterator;
-
 import org.usfirst.frc.team1732.robot.Robot;
 import org.usfirst.frc.team1732.robot.controlutils.GainProfile;
 import org.usfirst.frc.team1732.robot.controlutils.motionprofiling.pathing.Path;
@@ -9,7 +7,6 @@ import org.usfirst.frc.team1732.robot.controlutils.motionprofiling.pathing.Waypo
 import org.usfirst.frc.team1732.robot.subsystems.Drivetrain;
 
 import com.ctre.phoenix.motion.SetValueMotionProfile;
-import com.ctre.phoenix.motion.TrajectoryPoint;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
@@ -30,8 +27,8 @@ public class TestPathing extends Command {
 		path.addWaypoint(new Waypoint(0, 100, Math.PI / 2, 0));
 		path.generateProfile(Drivetrain.MAX_IN_SEC, Drivetrain.MAX_IN_SEC2);
 		System.out.println("Time to make: " + t.get());
-		path.setPathVars(Robot.drivetrain.leftFFF, Robot.drivetrain.rightFFF, 29,
-				0.01, 1.0 / Drivetrain.ENCODER_INCHES_PER_PULSE);
+		Path.setPathVars(Robot.drivetrain.leftFFF, Robot.drivetrain.rightFFF, 29, 0.01,
+				1.0 / Drivetrain.ENCODER_INCHES_PER_PULSE);
 		Robot.drivetrain.profileManager.reset(path.iterator());
 		GainProfile leftGain = Robot.drivetrain.leftGains.clone();
 		leftGain.setFF(Robot.drivetrain.leftFFF);
