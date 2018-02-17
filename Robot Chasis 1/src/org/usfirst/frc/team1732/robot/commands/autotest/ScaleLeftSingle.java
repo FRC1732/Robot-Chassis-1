2b1a86d47b271fb08b45f1ee9ffe9cdf04dae7da
Jay Schauer <lacrossejason@gmail.com>
2018-02-17 17:27:13 -0600

    added scale auto

--- Robot Chasis 1/src/org/usfirst/frc/team1732/robot/commands/autotest/ScaleLeftSingle.java
+++ Robot Chasis 1/src/org/usfirst/frc/team1732/robot/commands/autotest/ScaleLeftSingle.java
@@ -11,22 +11,22 @@ public class ScaleLeftSingle extends CommandGroup {
 	public ScaleLeftSingle(boolean scaleIsLeft) {
 		double startCenterX = Field.Switch.BOUNDARY.getX() - 5 - Drivetrain.ROBOT_WIDTH_IN / 2.0;
 		if (scaleIsLeft) {
-			double forwardDistance = Field.Scale.LEFT_NULL_ZONE.getY();
+			double forwardDistance = Field.Scale.LEFT_NULL_ZONE.getY() - Drivetrain.ROBOT_LENGTH_IN + 5;
 			System.out.println("forward1: " + forwardDistance);
 			addSequential(new DriveDistance(forwardDistance));
-			addSequential(new TurnAngle(-30, 80));
+			addSequential(new TurnAngle(35, 80));
 		} else {
-			double forwardDistance = Field.Switch.BOUNDARY.getMaxY() + Drivetrain.ROBOT_LENGTH_IN + 4;
+			double forwardDistance = Field.Switch.BOUNDARY.getMaxY() + 4;
 			System.out.println("forward1: " + forwardDistance);
-			double middlePartCenterY = forwardDistance - Drivetrain.ROBOT_LENGTH_IN / 2.0;
-			System.out.println("stopCenterY " + middlePartCenterY);
+			double middlePartY = forwardDistance + Drivetrain.ROBOT_LENGTH_IN;
+			System.out.println("stopCenterY " + middlePartY);
 			addSequential(new DriveDistance(forwardDistance));
 			addSequential(new TurnAngle(90, 80));
-			double forward2 = Field.Switch.BOUNDARY.getMaxX() - startCenterX + Drivetrain.ROBOT_LENGTH_IN;
+			double forward2 = Field.Switch.BOUNDARY.getMaxX() - startCenterX + 10;
 			System.out.println("forward2: " + forward2);
 			addSequential(new DriveDistance(forward2));
 			addSequential(new TurnAngle(-90, 90));
-			double forward3 = Field.Scale.RIGHT_NULL_ZONE.getMinY() - middlePartCenterY;
+			double forward3 = Field.Scale.RIGHT_NULL_ZONE.getMinY() - middlePartY;
 			System.out.println("forward3: " + forward3);
 			addSequential(new DriveDistance(forward3));
 			addSequential(new TurnAngle(-30, 80));
