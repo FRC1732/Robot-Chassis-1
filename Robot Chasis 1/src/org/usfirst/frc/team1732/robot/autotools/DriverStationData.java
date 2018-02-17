cbb80552663aa5fb1fc81343b9fc9cdabe9f90f0
the10thWiz <the10thwiz@gmail.com>
2018-02-17 17:39:53 -0600

    Wrote a scale right

--- Robot Chasis 1/src/org/usfirst/frc/team1732/robot/autotools/DriverStationData.java
+++ Robot Chasis 1/src/org/usfirst/frc/team1732/robot/autotools/DriverStationData.java
@@ -5,9 +5,9 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
 public class DriverStationData {
 
-	public static boolean closeSwitchIsLeft;
-	public static boolean scaleIsLeft;
-	public static boolean farSwitchIsLeft;
+	public static boolean closeSwitchIsLeft = false;
+	public static boolean scaleIsLeft = false;
+	public static boolean farSwitchIsLeft = false;
 	private static String platePosition = "";
 
 	public static boolean gotPlatePositions() {
